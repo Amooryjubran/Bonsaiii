7ffda4a53ec794e2961f1ff5bd6fb752c55954c3
Omar Gubran <54118716+Amooryjubran@users.noreply.github.com>
2023-01-28 18:50:00 -0500

    Set up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
