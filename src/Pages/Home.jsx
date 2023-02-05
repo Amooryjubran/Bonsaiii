328acfe09edbbc7e46e4ff419618eeacec939115
Omar Gubran <54118716+Amooryjubran@users.noreply.github.com>
2023-02-05 15:53:15 -0500

    HomePage | Categories Link

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,28 @@
 import { Fragment } from "react";
 import Slider from "@/Components/Slider/Slider";
 import { useQuery } from "../hooks/useQuery";
+import AppsIcon from "@mui/icons-material/Apps";
+import { Link } from "react-router-dom";
+import "./home.css";
+import Skeleton from "../utils/Skeleton";
 
 export default function Home() {
   const { data } = useQuery("plants");
   return (
     <Fragment>
-      <div>
+      <div className="SliderContainer">
+        <div className="sliderCategories">
+          {!!data.length ? (
+            <>
+              <AppsIcon />
+              <Link to="/shop">Categories</Link>
+            </>
+          ) : (
+            <div className="skeletonContainer">
+              <Skeleton height="150px" width="20px" />
+            </div>
+          )}
+        </div>
         <Slider props={data} />
       </div>
     </Fragment>
