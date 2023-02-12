05aeaac36ac8d31c2b4925dac9479113e6a963dc
amooryjubran <omarsalehjubran@gmail.com>
2023-02-12 11:33:51 -0500

    Layout | footer - navbar

--- src/Components/Layout/Footer/index.jsx
+++ src/Components/Layout/Footer/index.jsx
@@ -1,16 +1,18 @@
+import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import "./style.css";
+import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <footer>
-      <hr />
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span>Bonsaiii</span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <hr />
+      <div>
+        <Link to="https://github.com/Amooryjubran/Bonsaiii">
+          <GitHubIcon />
+        </Link>
+        <Link to="https://twitter.com/AmooryJu">
+          <TwitterIcon />
+        </Link>
+      </div>
     </footer>
   );
 }
