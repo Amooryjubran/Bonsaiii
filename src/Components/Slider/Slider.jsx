89ff2c2411e7294453635f1acc118147042910a8
Omar Gubran <54118716+Amooryjubran@users.noreply.github.com>
2023-02-27 22:26:21 -0500

    MobileCSS | Slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,13 +1,15 @@
 import Slider from "react-slick";
 import Skeleton from "../../utils/Skeleton";
 import ImageCard from "../ui/ImageCard";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import "./style.css";
 export default function Slide({ props, small }) {
+  const matches = useMediaQuery("(max-width:1028px)");
   var settings = {
     dots: false,
     infinite: false,
     speed: 500,
-    slidesToShow: small ? 1.5 : 3.5,
+    slidesToShow: small ? 1.5 : !matches ? 3.5 : 1.5,
     slidesToScroll: 1,
     initialSlide: 0,
     arrows: true,
