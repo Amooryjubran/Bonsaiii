f66aeede14e7b1055d972e18e03d0c354ac09cce
Omar Gubran <54118716+Amooryjubran@users.noreply.github.com>
2023-03-05 15:44:45 -0500

    Search | MobileCSS

--- src/Components/ui/Search/SearchSide.jsx
+++ src/Components/ui/Search/SearchSide.jsx
@@ -5,15 +5,17 @@ export default function SearchSide({ data, setSearch }) {
   return (
     <Fragment>
       <h1>Trending</h1>
-      {data.map((product) => (
-        <Link
-          to={`./shop/` + product.name.split(" ").join("+")}
-          key={product.id}
-          onClick={() => setSearch(false)}
-        >
-          {product.name}
-        </Link>
-      ))}
+      <ul>
+        {data.map((product) => (
+          <Link
+            to={`./shop/` + product.name.split(" ").join("+")}
+            key={product.id}
+            onClick={() => setSearch(false)}
+          >
+            {product.name}
+          </Link>
+        ))}
+      </ul>
     </Fragment>
   );
 }
