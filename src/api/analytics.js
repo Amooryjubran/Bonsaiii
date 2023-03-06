1828111b90df7627814d14ac6d424b483a0ccd6a
Omar Gubran <54118716+Amooryjubran@users.noreply.github.com>
2023-03-05 20:30:20 -0500

    Google Analytics | Env Fix

--- src/api/analytics.js
+++ src/api/analytics.js
@@ -1,9 +1,10 @@
 import ReactGA from "react-ga";
 
-const TRACKING_ID = VITE_API_KEY_TRACKING_ID;
 function init() {
   const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-  ReactGA.initialize(TRACKING_ID, { debug: isDev });
+  ReactGA.initialize(import.meta.env.VITE_API_KEY_TRACKING_ID, {
+    debug: isDev,
+  });
 }
 function sendEvent(payload) {
   ReactGA.event(payload);
